43dc14054c384cf25f8774fcb157e13a9fc8fc3b
Pushpendra Dwivedi <119349003+pushpendra-dwivedi-12@users.noreply.github.com>
2024-02-16 12:16:27 +0530

    Update arduino_secrets.h

--- Planir/arduino_secrets.h
+++ Planir/arduino_secrets.h
@@ -1,3 +1,3 @@
-#define SECRET_SSID ""
-#define SECRET_OPTIONAL_PASS ""
-#define SECRET_DEVICE_KEY ""
+#define SECRET_SSID "Hotspot"
+#define SECRET_OPTIONAL_PASS "12345678"
+#define SECRET_DEVICE_KEY "Secret Key" // provided by cloud plateform for communication between database server and IoT device
